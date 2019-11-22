75d8b33774e111de03bcf489d2c8c81234cbe169
IRCSS <sh.shahrabi@hotmail.com>
2019-11-22 14:06:23 +0100

    basic setup is finished

---
+++ Assets/Resources/points.shader
@@ -0,0 +1,65 @@
+﻿Shader "Unlit/points"
+{
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+		Cull Off
+
+		Pass
+		{
+			// =======================================================
+
+			CGPROGRAM
+			#pragma vertex   vert
+			#pragma fragment frag 
+			
+			#include "UnityCG.cginc"
+
+			struct v2f
+			{
+				float3 color  : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+	        StructuredBuffer<float4> _ParticleDataBuff;
+			                 float   _frameCount;
+            #define sin60 0.866025
+
+			float rand(float seed) {
+				return frac(sin(seed *1012.)*422.7);
+			}
+
+			v2f vert (uint id : SV_VertexID, uint inst : SV_InstanceID)
+			{
+				v2f o;
+				float3 pos   = _ParticleDataBuff[inst];
+
+				//pos.xy = float2(rand(inst + _frameCount) , rand(inst + 11.+ _frameCount))*2. -1.;
+
+				float3 right = float3(1, 0, 0);
+				float3 up    = float3(0, -1, 0);
+				float size   = 0.01;	
+
+				[branch] switch (id) {
+				case 0: pos = pos + right*size      ; break;
+				case 1: pos = pos - right*size      ; break;
+				case 2: pos = pos + up   *size*sin60; break;
+				};
+
+				pos     += -up*size*sin60*0.5;
+				o.color  = lerp( float3(1., 0., 0.), float3(0., 1., 0.), step(1.0, length(pos.xy)));
+				o.vertex = float4(pos, 1.);
+				
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = i.color.xyzz;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/scripts/MonteCarloSampler.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MonteCarloSampler : MonoBehaviour {
+
+    public  int           numberOfSamplesPerPass;
+
+
+    private CommandBuffer cbDrawPoints;
+    private Mesh          toDraw;
+    private Vector4[]     points;
+    private ComputeBuffer computerBufferpoints;
+    private RenderTexture screenTexture;
+    private Material      mt;
+    private Camera        mainCm;
+    private CommandBuffer cb;
+	// Use this for initialization
+	void Start () {
+
+
+        screenTexture        = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        computerBufferpoints = new ComputeBuffer(numberOfSamplesPerPass, sizeof(float) * 4);
+        points               = new Vector4[numberOfSamplesPerPass];
+        mt                   = new Material(Shader.Find("Unlit/points"));
+
+       
+
+    
+
+        mainCm  = Camera.main;
+        cb      = new CommandBuffer();
+        cb.name = "sampling";
+
+        cb.Blit(screenTexture, BuiltinRenderTextureType.CameraTarget);
+        mainCm.AddCommandBuffer(CameraEvent.AfterEverything, cb); 
+  
+
+    }
+	
+
+     void Resample()
+    {
+        for (int i = 0; i < numberOfSamplesPerPass; i++)
+        {
+            //float width = Mathf.Floor(Mathf.Sqrt(numberOfSamplesPerPass));
+            //float x = i % width;
+            //float y = Mathf.Floor(i / width);
+            //points[i] = new Vector4((x / width) * 2f - 1f, (y / width) * 2f - 1f, 0.5f, 0f);
+
+            float x = Random.Range(-1f, 1f);
+            float y = Random.Range(-1f, 1f);
+
+            points[i] = new Vector4(x, y, 0.5f, 0f);
+
+            
+            
+        }
+        computerBufferpoints.SetData(points);
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        Resample();
+        Graphics.SetRenderTarget(screenTexture);
+        mt.SetPass(0);
+        mt.SetFloat ("_frameCount", (float) Time.frameCount);
+        mt.SetBuffer("_ParticleDataBuff", computerBufferpoints);
+        Graphics.DrawProcedural(MeshTopology.Triangles, 3, numberOfSamplesPerPass);
+        Graphics.SetRenderTarget(null);
+    }
+}
